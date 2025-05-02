9c114d58e1cc5a49d4b1632d7e9de21e9704e982
Oleksandr Kovaliuk <o.kovaliuk@steel.mil.ua>
2025-05-02 12:57:57 +0300

    Add first scratch

--- src/helpers/store.ts
+++ src/helpers/store.ts
@@ -10,10 +10,6 @@ const saveAction = (id, isCheck) => {
 const getIsCheckAction = (id) => {
   const store = getStore();
 
-  if (id === "02.05.2025-washEyes-0") {
-    console.log("id", id, JSON.parse(store), JSON.parse(store)?.[id]);
-  }
-
   return JSON.parse(store)?.[id];
 };
 
