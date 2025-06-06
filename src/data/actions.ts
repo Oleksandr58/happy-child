d71706fcfe2b0b4fe0f570f327f98dd1412818d3
Oleksandr Kovaliuk <o.kovaliuk@steel.mil.ua>
2025-06-06 23:41:11 +0300

    Deploy v1.7

--- src/data/actions.ts
+++
@@ -1,7 +0,0 @@
-import regularActions from "./regularActions";
-import iregularActions from "./iregularActions";
-import { action } from '../types/action';
-
-const ACTIONS: action[] = [...regularActions, ...iregularActions];
-
-export default ACTIONS;
